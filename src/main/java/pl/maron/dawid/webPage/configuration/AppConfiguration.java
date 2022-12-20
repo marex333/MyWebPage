f4791ba714f299714681446efffb6fc734e6ce65
Dawid Maroń <daw.maron@gmail.com>
2022-12-20 13:47:51 +0100

    init

---
+++ src/main/java/pl/maron/dawid/webPage/configuration/AppConfiguration.java
@@ -0,0 +1,10 @@
+package pl.maron.dawid.webPage.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("pl.maron.dawid")
+public class AppConfiguration {
+
+}
---
+++ src/main/java/pl/maron/dawid/webPage/controllers/CommonController.java
@@ -0,0 +1,13 @@
+package pl.maron.dawid.webPage.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class CommonController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String main(){
+        return "main";
+    }
+}
