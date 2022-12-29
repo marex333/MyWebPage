c415a0236290506d12549b34ed988f8905526a7d
Dawid Maroń <daw.maron@gmail.com>
2022-12-29 22:25:01 +0100

    update

--- src/main/java/pl/maron/dawid/webPage/controllers/CommonController.java
+++ src/main/java/pl/maron/dawid/webPage/controllers/CommonController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class CommonController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String main(){
-        return "main2";
+        return "main";
     }
 
     @RequestMapping(value = "/about", method = RequestMethod.GET)
